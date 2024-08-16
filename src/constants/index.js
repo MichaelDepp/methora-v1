91b20c678027f008d0e75d013c002e3547ba0f9c
michaeldepp <michaeldepp7@gmail.com>
2024-08-17 01:17:23 +0800

    feat : add the color constant file

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+// We are storing the color values of the theme in this constant
+// You can change the color themes here directly, and see the changes instanty in the page
+module.exports = {
+  colors: {
+    theme: {
+      primary: '#F5A8FA',
+      secondary: '#262626',
+      bg: '#121315',
+      lightBg: '#fefefe',
+      dark: '#16171b',
+      darkLight: '#1E1F23',
+    },
+  },
+};
